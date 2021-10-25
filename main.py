093c7f7cf5cc0396cb5f3dc3aff8148299c0d84d
Dmitri <46726765+tdmitri@users.noreply.github.com>
2021-10-25 14:17:13 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# pip install easyocr
+import easyocr
+
+
+def text_recognition(file_path, text_file_name="result.txt"):
+    reader = easyocr.Reader(["ru", "en"])
+    result = reader.readtext(file_path, detail=0, paragraph=True)
+    
+    with open(text_file_name, "w") as file:
+        for line in result:
+            file.write(f"{line}\n\n")
+    
+    return f"Result wrote into {text_file_name}"
+
+
+def main():
+    file_path = input("File path: ")
+    print(text_recognition(file_path=file_path))
+    
+    
+if __name__ == "__main__":
+    main()
